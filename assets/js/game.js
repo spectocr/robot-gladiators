588cd44e23b1fc7a503857fe79205d8f6078b060
spectocr <highstakes22@gmail.com>
2021-04-27 11:19:54 -0400

    finish 3.2.7

--- assets/js/game.js
+++ assets/js/game.js
@@ -16,11 +16,30 @@ var enemyAttack = 12;
 // fight function - ??can we explain this differently? why make it a var?
 var fight = function(enemyName) {
   // Alert players that they are starting the round
-  window.alert("Welcome to Robot Gladiators!");
-
+  //window.alert("Welcome to Robot Gladiators!");
+// repeat and execute as long as the enemy-robot is alive
+  while(playerHealth > 0 && enemyHealth > 0) {
   // ask player if they'd like to fight or run
   var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+// if player picks "skip" confirm and then stop the loop
+if (promptFight === "skip" || promptFight === "SKIP") {
+  // confirm player wants to skip
+  var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
+    // if yes (true), leave fight
+    if (confirmSkip) {
+      window.alert(playerName + " has decided to skip this fight. Goodbye!");
+      // subtract money from playerMoney for skipping
+      playerMoney = playerMoney - 2;
+      console.log("playerMoney", playerMoney)
+      break;
+    }
+    // if no (false), ask question again by running fight() again
+    else {
+      fight();
+    }
+    // if player did not chose 1 or 2 in prompt
+  } 
   // if player choses to fight, fight
   if (promptFight === "fight" || promptFight === "FIGHT") {
     // remove enemy's health by subtracting the amount set in the playerAttack variable
@@ -28,12 +47,16 @@ var fight = function(enemyName) {
     console.log(
       playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
     );
-
+    if (playerHealth <= 0) {
+      window.alert(playerName + " has died!");
+      break;
+    }
     // check enemy's health -- question here whats the deal with structure? space indents, etc. does it matter on placement like it does in CSS?
     if (enemyHealth <= 0) {
       window.alert(enemyName + " has died!");
+      break;
     } else {
-      window.alert(enemyName + " still has " + enemyHealth + " health left.");
+      //window.alert(enemyName + " still has " + enemyHealth + " health left.");
     }
 
     // remove players's health by subtracting the amount set in the enemyAttack variable
@@ -44,31 +67,22 @@ var fight = function(enemyName) {
 
     // check player's health
     if (playerHealth <= 0) {
-      window.alert(playerName + " has died!");
+      //window.alert(playerName + " has died!");
     } else {
-      window.alert(playerName + " still has " + playerHealth + " health left.");
+      //window.alert(playerName + " still has " + playerHealth + " health left.");
     }
     // if player choses to skip
-  } else if (promptFight === "skip" || promptFight === "SKIP") {
-    // confirm player wants to skip
-    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-
-    // if yes (true), leave fight
-    if (confirmSkip) {
-      window.alert(playerName + " has decided to skip this fight. Goodbye!");
-      // subtract money from playerMoney for skipping
-      playerMoney = playerMoney - 2;
-    }
-    // if no (false), ask question again by running fight() again
-    else {
-      fight();
-    }
-    // if player did not chose 1 or 2 in prompt
-  } else {
+  }
+  
+  else {
     window.alert("You need to pick a valid option. Try again!");
   }
+}
 };
-
+// how do i understand the difference between the while and for loops? why use one over the other?
 for(var i = 0; i < enemyNames.length; i++) {
+  var pickedEnemyName = enemyNames[i];
+  enemyHealth  = 50;
+  // call fight function with enemy-robot
     fight(enemyNames[i]);
   }
